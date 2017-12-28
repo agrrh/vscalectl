e00ac1fe6db81086fdd9935315f2a372e8e0f42a
Kirill K <kovalev.kirill.a@gmail.com>
2017-12-28 03:45:21 +0300

    Add detailed usage info

--- vscalectl.py
+++ vscalectl.py
@@ -9,8 +9,31 @@ from lib.api import API
 from lib.client import Client
 
 
-def args_parse():
-    parser = argparse.ArgumentParser(description='CLI tool to operate vscale.io API')
+epilog = """usage:
+
+  vscalectl account                  Display account info.
+            locations                Show available data-centers.
+            images                   List OS repository.
+            plans                    Inspect resources and prices.
+
+            servers                  Show all servers.
+            servers <ID>             Get single server information.
+            servers <Name> create    Create new server with specific name and hostname.
+            servers <ID> stop        Stop (shutdown) server.
+            servers <ID> start       Turn server on.
+            servers <ID> restart     Reboot server.
+            servers <ID> rebuild     Reinstall server from original image.
+            servers <ID> delete      Permanently remove server.
+
+  Please visit control panel on https://vscale.io/panel/ to perform more complex actions.
+"""
+
+def args_parse(epilog):
+    parser = argparse.ArgumentParser(
+        description='CLI tool to operate vscale.io API.',
+        epilog=epilog,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
 
     parser.add_argument(
         'object', metavar='OBJECT', type=str,
@@ -28,7 +51,7 @@ def args_parse():
     parser.add_argument('--no-header', action='store_true', help='Do not print header in lists.')
 
     parser.add_argument('--cache-file', type=str, default='~/.vscalectl.yml', help='Path of file to store cached default parameters.')
-    parser.add_argument('--no-cache', action='store_true', help='Remove cache file before running script')
+    parser.add_argument('--no-cache', action='store_true', help='Remove cache file before running script.')
 
     return parser, parser.parse_args()
 
@@ -39,7 +62,7 @@ if __name__ == '__main__':
         print('Please set "VSCALE_API_TOKEN" variable to be able to call API. Exiting.')
         sys.exit(1)
 
-    parser, args = args_parse()
+    parser, args = args_parse(epilog)
 
     cache = Cache(args.cache_file, dont_use_cache=args.no_cache)
     cache.load()
