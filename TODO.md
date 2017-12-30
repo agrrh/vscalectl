008c027c1ade39328bc470964ca46a97c2afea7c
Kirill K <kovalev.kirill.a@gmail.com>
2017-12-30 11:57:59 +0300

    Improving build

--- TODO.md
+++ TODO.md
@@ -1,6 +1,7 @@
 Things not to forget:
 
-- Create Telegram group/channel to be able to chat about the product
+- Create Telegram group/channel to be able to chat about the product (gitter?)
+- Prepare image for build
 - Write an article
 - Implement keys management and usage while creating servers
 - Reuse image for smoke/functional tests
---
+++ util/build_name_generate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+LATEST_TAG=$(curl -s https://api.github.com/repos/agrrh/vscalectl/tags | jq -r '.[0]["name"]')
+LATEST_SHA=$(curl -s https://api.github.com/repos/agrrh/vscalectl/commits | jq -r '.[0]["sha"]' | head -c 8)
+
+# ex. v1.0.1-alpha
+VERSION_API=$(echo -n ${LATEST_TAG} | cut  -d '.' -f1)
+VERSION_MAJOR=$(echo -n ${LATEST_TAG} | cut  -d '.' -f2)
+VERSION_MINOR=$(echo -n ${LATEST_TAG} | cut  -d '-' -f1 | cut  -d '.' -f3)
+
+NEW_MINOR=$[${VERSION_MINOR}+1]
+
+NEW_TAG="${VERSION_API}.${VERSION_MAJOR}.${NEW_MINOR}"
+
+export VSCALECTL_BUILD=${NEW_TAG}-pre-${LATEST_SHA}
